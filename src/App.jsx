baae63a5c1b126536455525a2b3e4952b39547d3
rcmehedi15 <hmdmehedi2016@gmail.com>
2023-05-08 10:03:50 +0600

    done

--- src/App.jsx
+++ src/App.jsx
@@ -9,17 +9,46 @@ function App() {
     .then(res => res.json())
     .then(data => setUsers(data));
   },[])
+// user handle
+const handleAddUser = event => {
+  event.preventDefault();
+  const form = event.target;
+  const name = form.name.value;
+  const email = form.email.value;
+  const user = { name, email}
+  console.log(user);
+  fetch(`https://user-management-server-gamma.vercel.app/users`, {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json'
+    },
+    body: JSON.stringify(user)
+  })
+  .then(res => res.json())
+  .then(data => {
+    console.log(data);
+    const newUsers = [...users, data]
+    setUsers(newUsers);
+    form.reset();
+  });
+
+}
   return (
     <>
       <h1>Users Management System</h1>
       <h3>Numbers of Users: {users.length}</h3>
-      <form >
-        <input type="text" name="name" id=""  placeholder='Name'/>
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" id=""  placeholder='Name' required/>
         <br />
-        <input type="email" name="email" id="" placeholder='Email'/>
+        <input type="email" name="email" id="" placeholder='Email' required/>
         <br />
         <input type="submit" value="Add User" />
       </form>
+      <div>
+        {
+          users.map(user => <p key={user.id}>{user.id} : {user.name} : {user.email}</p>)
+        }
+      </div>
     </>
   )
 }
