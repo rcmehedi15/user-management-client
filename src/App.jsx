fca015f235311a9b05f2e07b713b4ea1928da413
rcmehedi15 <hmdmehedi2016@gmail.com>
2023-05-08 09:46:38 +0600

    sever data to client fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-
+  const [users,setUsers] = useState([]);
+  useEffect(() => {
+    fetch(`https://user-management-server-gamma.vercel.app/users`)
+    .then(res => res.json())
+    .then(data => setUsers(data));
+  },[])
   return (
     <>
       <h1>Users Management System</h1>
+      <h3>Numbers of Users: {users.length}</h3>
     </>
   )
 }
